3171a90522c646166cba8756e3bb7683620b1ed6
Sajal Shrestha <codeitsajal@gmail.com>
2021-04-08 21:53:30 +0545

    login and register added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:muse_app/api/api.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -6,6 +9,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  GlobalKey<FormState> _key = GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -14,56 +20,120 @@ class _LoginScreenState extends State<LoginScreen> {
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                children: [
-                  //logo
-                  FlutterLogo(
-                    size: 50,
-                  ),
-                  //Email
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.email),
-                      labelText: 'Email',
+              child: Form(
+                key: _key,
+                child: Column(
+                  children: [
+                    //logo
+                    FlutterLogo(
+                      size: 50,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  //Password
-                  TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        labelText: 'Your secret password',
-                        suffixIcon: IconButton(
-                            icon: Icon(Icons.visibility), onPressed: () {})),
-                    keyboardType: TextInputType.emailAddress,
-                    obscureText: true,
-                  ),
-                  //Login Button
-                  Row(
-                    children: [
-                      Expanded(
-                          child: ElevatedButton(
-                              onPressed: () {}, child: Text("LOGIN")))
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  RichText(
-                    text: TextSpan(
+                    //Email
+                    TextFormField(
+                      controller: email,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        labelText: 'Email',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    //Password
+                    TextFormField(
+                      controller: password,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          labelText: 'Your secret password',
+                          suffixIcon: IconButton(
+                              icon: Icon(Icons.visibility), onPressed: () {})),
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: true,
+                    ),
+                    //Login Button
+                    Row(
                       children: [
-                        TextSpan(
-                            text: 'Are you new?',
-                            style: TextStyle(color: Colors.black)),
-                        TextSpan(
-                            text: ' Register here',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600))
+                        Expanded(
+                            child: ElevatedButton(
+                                onPressed: () async {
+                                  if (_key.currentState.validate()) {
+                                    Map data = {
+                                      "email": email.text,
+                                      "password": password.text
+                                    };
+
+                                    var response = await Api()
+                                        .loginAndRegister(data, 'login');
+                                    var result = json.decode(response.body);
+                                    print(result);
+                                    if (result['message'] ==
+                                        "Unauthorized user") {
+                                      showDialog(
+                                          context: context,
+                                          builder: (builder) {
+                                            return AlertDialog(
+                                              title: Text("Message"),
+                                              content:
+                                                  Text("Unauthorized user"),
+                                              actions: [
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Text("OK"))
+                                              ],
+                                            );
+                                          });
+                                    }
+
+                                    if (result['user']['is_admin'] == 0) {
+                                      Navigator.popAndPushNamed(
+                                          context, 'dashboard');
+                                    } else {
+                                      showDialog(
+                                          context: context,
+                                          builder: (builder) {
+                                            return AlertDialog(
+                                              title: Text("Message"),
+                                              content: Text(
+                                                  "Welcome to Employee Dashboard"),
+                                              actions: [
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Navigator.pop(context);
+                                                    },
+                                                    child: Text("OK"))
+                                              ],
+                                            );
+                                          });
+                                    }
+                                  }
+                                },
+                                child: Text("LOGIN")))
                       ],
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, 'register');
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                                text: 'Are you new?',
+                                style: TextStyle(color: Colors.black)),
+                            TextSpan(
+                                text: ' Register here',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600))
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
